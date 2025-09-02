329c830a76df0ccd7f841bbdff0ed19f0513e53b
JDFerro <ferrojuan2003@gmail.com>
2025-09-02 18:35:47 -0500

    añadimos la redireccion a la vista resumenorden

--- src/main/java/com/curso/ecommerce/controller/HomeController.java
+++ src/main/java/com/curso/ecommerce/controller/HomeController.java
@@ -121,4 +121,12 @@ public class HomeController {
         return "/usuario/carrito";
     }
 
+
+    @GetMapping("/order")
+    public String order(){
+
+        return "usuario/resumenorden";
+    }
+
+
 }
