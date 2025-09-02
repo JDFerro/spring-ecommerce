9f5919a32f19fa8a1eb3cb601607f1695b432102
JDFerro <ferrojuan2003@gmail.com>
2025-09-02 17:14:48 -0500

    Redireccion al carrito de compras

--- src/main/java/com/curso/ecommerce/controller/HomeController.java
+++ src/main/java/com/curso/ecommerce/controller/HomeController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.slf4j.Logger;
 
@@ -40,4 +41,10 @@ public class HomeController {
 
         return "usuario/productohome";
     }
+
+    @PostMapping("/cart")
+    public  String addCart(){
+        return "usuario/carrito";
+    }
+
 }
